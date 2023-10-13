c73d423f86b3a284a20447652918f016681e3196
ygdalparslan <ygdalparslan@gmail.com>
2023-10-13 17:34:10 +0300

    JavaInheritanceExercises.p03

---
+++ src/main/java/JavaInheritanceExercises/p03/Runner.java
@@ -0,0 +1,12 @@
+package JavaInheritanceExercises.p03;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle(3.0,5.0);
+
+        double area =rectangle.getArea();
+        System.out.println("Rectangle Area= "+area);
+
+    }
+}
---
+++ src/main/java/JavaInheritanceExercises/p03/Shape.java
@@ -0,0 +1,13 @@
+package JavaInheritanceExercises.p03;
+/*
+3. Write a Java program to create a class called Shape with a method called getArea().
+Create a subclass called Rectangle that overrides the getArea() method to calculate the area of a rectangle.
+ */
+
+public class Shape {
+
+    public double getArea(){
+        return 0.0;
+    }
+
+}
