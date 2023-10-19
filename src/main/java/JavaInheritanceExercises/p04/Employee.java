11d706ac86ca75abb40d022eb4c19520b394c98b
ygdalparslan <ygdalparslan@gmail.com>
2023-10-19 22:07:08 +0300

    JavaInheritanceExercises.p04

---
+++ src/main/java/JavaInheritanceExercises/p04/Employee.java
@@ -0,0 +1,15 @@
+package JavaInheritanceExercises.p04;
+
+/*
+4. Write a Java program to create a class called Employee with methods called work() and getSalary().
+Create a subclass called HRManager that overrides the work() method and adds a new method called addEmployee().
+ */
+
+public class Employee {
+
+    public void work(){
+
+
+    }
+
+}
